302ea41235bd238016a432736f9e6fb3fb41457b
sokhona <diaksokhona1993@gmail.com>
2022-03-11 19:57:18 +0100

    first commit

---
+++ src/QuestionForm.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+function QuestionForm() {
+    const [inputValue, setInputValue] = useState('Posez votre question ici')
+    return (
+        <div>
+    <textarea
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+    />
+    <button onClick={() => alert(inputValue)}>Alertez moi 🚨</button>
+           
+        </div>
+    )
+}
+
+
+function handleSubmit(e) {
+    e.preventDefault()
+    alert(e.target['my_input'].value)
+}
+export default QuestionForm
